2628cfa0bfe52c9b1ee1c8e165f5eed49d22d444
paulc <paulcheater@gmail.com>
2022-04-04 19:41:07 +0200

    Added comments in scrapper/utils.py

--- src/scrapping/utils.py
+++ src/scrapping/utils.py
@@ -1,7 +1,10 @@
 from typing import Any
 
+
 def to_float(number: Any) -> float | None:
     try:
         return float(str(number).replace(',', '.'))
     except ValueError as e:
+        #  FIXME LOGGING {e}
+        print(f"Float conversion error: {e}") # FIXME CHANGE TO LOGGER
         return None
