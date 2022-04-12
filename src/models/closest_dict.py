d429b52780d619c16ca79ddd32706bb2f215f085
paulc <paulcheater@gmail.com>
2022-04-12 18:25:30 +0200

    Code cleanup in src/models/closest_dict.py

--- src/models/closest_dict.py
+++ src/models/closest_dict.py
@@ -1,85 +1,77 @@
 from typing import Any, Callable
 from functools import wraps
 import sys
-import math
 from enum import Enum
 # from collections.abc import MutableMapping
 
 
 class ClosestDict(dict):
 
+    @classmethod
     def __new__(cls, *args, **kwargs):
         if(
-            (args and type(args) is dict
-             # (args and all(list(map(lambda a: type(a) is dict, args)))
-             #  and any(list(map(lambda k: type(k) is not int, args.keys()))))
-             and any(list(map(lambda k: type(k) is not int, args.keys()))))
+            (
+                args and type(args) is dict
+                and any(list(map(lambda k: type(k) not in (int, float), args.keys())))
+            )
             or
-            (kwargs and type(kwargs) is dict
-             and any(list(map(lambda k: type(k) is not int, kwargs.keys()))))
+            (
+                kwargs and type(kwargs) is dict
+                and any(list(map(lambda k: type(k) not in (int, float), kwargs.keys())))
+            )
         ):
-            raise ValueError("Dictionary accepts integer keys only.")
+            raise ValueError("Dictionary accepts number keys only (int | float).")
         return super(ClosestDict, cls).__new__(cls)
-        # return super(AirQualityIndexDict, cls).__new__(cls, *args, **kwargs)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.lower_bound: int = ~sys.maxsize
+        self.lower_bound: int = ~sys.maxsize  # FIXME?!
         self.upper_bound: int = sys.maxsize
 
+    @staticmethod
     def _is_key_valid(func: Callable) -> Callable:
         wraps(func)
 
-        def wrapper(self, key: int | None, value: Any | None = None):  # FIXME none czy exception
-            # if key and type(key) is not int:
-            if type(key) is not int:
-                raise ValueError("Dictionary accepts integer keys only.")
+        def wrapper(self, key: int | float | None, value: Any | None = None) -> Any:
+            if type(key) not in (int, float):
+                raise ValueError("Dictionary accepts integer and float keys only.")
             elif key < self.lower_bound:
-                raise KeyError(f"Given key: ({key}) is less than the lower bound: ({self.lower_bound})")
+                raise KeyError(
+                    f"Given key: ({key}) is less than the lower bound: ({self.lower_bound})"
+                )
             if func.__name__ == "__setitem__":
                 return func(self, key, value)
             return func(self, key)
         return wrapper
-        # FIXME
-        # if callable(func):  # FIXME
-        #     wraps(func)
-        #     return wrapper
-        # else:
-        #     raise NotImplementedError()
-        #     pass  # FIXME
 
-    def _get_closest_key_ceiling(self, key: int) -> int | None:
+    def _get_closest_key_ceiling(self, key: int | float) -> int | None:
+
         keys: list[int] = sorted(self.keys())
         last_key_idx: int = len(keys) - 1
-        for i, k in enumerate(keys):
-            if key < k:
-                return k
-            else:
-                if i < last_key_idx:
-                    continue
-                else:
-                    return k
+        return next(
+            (k for i, k in enumerate(keys) if key >= k and i >= last_key_idx or key < k),
+            None
+        )
 
     @_is_key_valid
-    def __getitem__(self, key: int):
+    def __getitem__(self, key: int | float):
         if len(self) == 0:
             raise KeyError(f"Cannot get value for key: ({key}) from an empty dictionary.")
-        new_key: int = self._get_closest_key_ceiling(key)
+        new_key: int | None = self._get_closest_key_ceiling(key)
         return super(ClosestDict, self).__getitem__(new_key)
 
     @_is_key_valid
-    def __setitem__(self, key: int, value: Any):
+    def __setitem__(self, key: int | float, value: Any):
         return super(ClosestDict, self).__setitem__(key, value)
 
-    # FIXME? remove this?
     @_is_key_valid
-    def __delitem__(self, key: int):
+    def __delitem__(self, key: int | float):
         if key not in self:
             key = self.__getitem__(key)
         super(ClosestDict, self).__delitem__(key)
 
     def __str__(self) -> str:
-        sorted_index_dict: dict[int, Any] = dict(sorted(self.items(), key=lambda k: k[0]))
+        sorted_index_dict: dict[int | float, Any] = dict(sorted(self.items(), key=lambda k: k[0]))
         return f"{sorted_index_dict}"
 
     def __repr__(self):
@@ -92,7 +84,7 @@ class ClosestDict(dict):
 
 class AirQualityIndexDict(ClosestDict):
 
-    class AirQualityIndexScale(Enum):
+    class AirQualityIndexScale(Enum):  # FIXME ADD A POLISH MAPPING?
         EXTREMELY_POOR = 0
         VERY_POOR = 1
         POOR = 2
@@ -106,6 +98,7 @@ class AirQualityIndexDict(ClosestDict):
         def __lt__(self, other) -> bool:
             return self.value < other.value
 
+        @staticmethod
         def _check_args(func: Callable) -> Callable:
             wraps(func)
 
@@ -159,7 +152,7 @@ class AirQualityIndexDict(ClosestDict):
                 "AirQualityIndexDict.AirQualityIndexScale values only."
             )
         super(AirQualityIndexDict, self).__init__(*args, **kwargs)
-        self.lower_bound: int = 0
+        self.lower_bound: float = 0.0
 
     @staticmethod
     def get_air_quality_index(*args):
