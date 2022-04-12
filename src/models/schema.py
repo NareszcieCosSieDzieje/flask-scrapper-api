deacb0f382e9a0f23ce037f8f7b9645c26b72429
paulc <paulcheater@gmail.com>
2022-04-12 18:24:26 +0200

    Code cleanup in src\models\schema.py plus fixed relative imports

--- src/models/schema.py
+++ src/models/schema.py
@@ -1,5 +1,14 @@
 from peewee import DatabaseProxy, Model, TextField, FloatField, FixedCharField, DateTimeField
-from closest_dict import AirQualityIndexDict
+import logging
+
+logger = logging.getLogger(__name__)
+
+try:
+    from closest_dict import AirQualityIndexDict
+except ImportError as ie:
+    # relative import if run from other package with the same level in the hierarchy
+    logger.warning(f"Could perform an import of a module inside a package, using a relative import.{ie}")
+    from .closest_dict import AirQualityIndexDict
 
 sqlite_db = DatabaseProxy()
 
@@ -9,7 +18,7 @@ class BaseModel(Model):
         database = sqlite_db
 
 
-# TODO: JAKIES ID NA SITE?!
+# TODO: ID FOR SITE?
 class Smog(BaseModel):
     site = TextField(null=False)
     air_quality_index = TextField(default=None)
@@ -31,48 +40,44 @@ class Smog(BaseModel):
         formats=r"%Y-%m-%d %H:%M:%S",
         null=False
     )
-    ''' 2022-02-05 15:22:12 ''' # FIXME
 
-    def __str__(self): # FIXME
-        #  FIXME? return f"{vars(self)}"
-        smog_str: str = \
-            f"""
-            {self.site=}
-            {self.air_quality_index=}
-            {self.measurement_timestamp=}
-            {self.PM10=} {self.PM10_unit}
-            {self.PM2_5=} {self.PM2_5_unit}
-            {self.O3=} {self.O3_unit}
-            {self.NO2=} {self.NO2_unit}
-            {self.SO2=} {self.SO2_unit}
-            {self.C6H6=} {self.C6H6_unit}
-            {self.CO=} {self.CO_unit}
-            """
-        return smog_str
+    def __eq__(self, other) -> bool:
+        return (
+            self.site == other.site and
+            self.air_quality_index == other.air_quality_index and
+            self.PM10 == other.PM10 and
+            self.PM2_5 == other.PM2_5 and
+            self.O3 == other.O3 and
+            self.NO2 == other.NO2 and
+            self.SO2 == other.SO2 and
+            self.C6H6 == other.C6H6 and
+            self.CO == other.CO and
+            self.PM10_unit == other.PM10_unit and
+            self.PM2_5_unit == other.PM2_5_unit and
+            self.O3_unit == other.O3_unit and
+            self.NO2_unit == other.NO2_unit and
+            self.SO2_unit == other.SO2_unit and
+            self.C6H6_unit == other.C6H6_unit and
+            self.CO_unit == other.CO_unit and
+            self.measurement_timestamp == other.measurement_timestamp
+        )
 
-    def __repr_(self): # FIXME
-        #  FIXME? return f"{vars(self)}"
-        smog_str: str = \
-            f"""
-            {self.site=}
-            {self.air_quality_index=}
-            {self.measurement_timestamp=}
-            {self.PM10=}
-            {self.PM2_5=}
-            {self.O3=}
-            {self.NO2=}
-            {self.SO2=}
-            {self.C6H6=}
-            {self.CO=}
-            {self.PM10_unit=}
-            {self.PM2_5_unit=}
-            {self.O3_unit=}
-            {self.NO2_unit=}
-            {self.SO2_unit=}
-            {self.C6H6_unit=}
-            {self.CO_unit=}
-            """
-        return smog_str
+    def __str__(self) -> str:
+        return (
+            f"{self.site=}\n"
+            f"{self.air_quality_index=}\n"
+            f"{self.measurement_timestamp=}\n"
+            f"{self.PM10=} {self.PM10_unit}\n"
+            f"{self.PM2_5=} {self.PM2_5_unit}\n"
+            f"{self.O3=} {self.O3_unit}\n"
+            f"{self.NO2=} {self.NO2_unit}\n"
+            f"{self.SO2=} {self.SO2_unit}\n"
+            f"{self.C6H6=} {self.C6H6_unit}\n"
+            f"{self.CO=} {self.CO_unit}\n"
+        )
+
+    def __repr__(self) -> str:
+        return f"{vars(self)}"
 
     def get_air_quality_index(self) -> str | None:
         # Taken in 2022 from | "https://airindex.eea.europa.eu/Map/AQI/Viewer/#"
@@ -132,7 +137,7 @@ class Smog(BaseModel):
             }
         )
 
-        # FIXME ADD THESE VALUES?
+        # TODO:? so far no thresholds found for these pollutants
         # c6h6_treshold: AirQualityIndexDict = AirQualityIndexDict(
         #     {
         #         "good": 0,
@@ -177,20 +182,31 @@ class Smog(BaseModel):
                     pollutant_tresholds[self_pollutant]
                 air_quality_descriptions.append(pollutant_air_quality_description)
 
-        # FIXME SET QUALITY INDEX?!
         air_quality_index: AirQualityIndexDict.AirQualityIndexScale = \
             AirQualityIndexDict.get_air_quality_index(*air_quality_descriptions)
 
         if air_quality_index:
-            return air_quality_index.name  # TODO: lowercase?
+            return air_quality_index.name.replace('_', ' ')  # TODO: lowercase?
+
+    def set_air_quality_index(self) -> None:
+        self.air_quality_index = self.get_air_quality_index()
+
+
+def smog_factory(*args, **kwargs) -> Smog:
+    smog: Smog = Smog(*args, **kwargs)
+    smog.set_air_quality_index()
+    return smog
 
 
 class Email(BaseModel):
+    # TODO: ...
     email = TextField(null=False)
 
 
 def main() -> None:
-    smog: Smog = Smog(  # FIXME CHANGE EXAMPLARY VALUES?
+    print('-' * 40)
+    print('Smog1')
+    smog1: Smog = Smog(
         PM10=12,
         PM2_5=800,
         O3=291,
@@ -199,8 +215,24 @@ def main() -> None:
         C6H6=1,
         CO=40,
     )
-    air_quality_index: str = smog.get_air_quality_index()
+    air_quality_index: str | None = smog1.get_air_quality_index()
     print(f"{air_quality_index = }")
+    print(f"\nRepr of smog: {smog1!r}")
+    print('-' * 40)
+    # equivalent to
+    smog2: Smog = smog_factory(
+        PM10=12,
+        PM2_5=800,
+        O3=291,
+        NO2=20,
+        SO2=2,
+        C6H6=1,
+        CO=40,
+    )
+    print('\n')
+    print('-' * 40)
+    print('Smog2')
+    print(vars(smog2))
 
 
 if __name__ == '__main__':
