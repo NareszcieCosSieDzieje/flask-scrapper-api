d5be3c3b30c67bffaaa0a6411d80ab91eb4e3845
paulc <paulcheater@gmail.com>
2022-04-22 16:04:34 +0200

    Added a simple config class for Flask

---
+++ src/flask_config.py
@@ -0,0 +1,17 @@
+
+
+class Config:
+    SCHEDULER_API_ENABLED = True
+    # DEBUG = True
+    DEBUG = False
+
+# app.config.update(
+#     TESTING=True,
+#     SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/'
+# )
+# app.config['MAIL_SERVER']='smtp.mailtrap.io'
+# app.config['MAIL_PORT'] = 2525
+# app.config['MAIL_USERNAME'] = '97e041d5e367c7'
+# app.config['MAIL_PASSWORD'] = 'cfaf5b99f8bafb'
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USE_SSL'] = False
