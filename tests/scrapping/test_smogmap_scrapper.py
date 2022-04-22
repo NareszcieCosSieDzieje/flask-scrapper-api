1b270cdff4ab3ac8112bbc95e02c3620f97d6c94
paulc <paulcheater@gmail.com>
2022-04-22 15:55:21 +0200

    Added NotImplementedError in 'tests/scrapping/test_smogmap_scrapper.py'

--- tests/scrapping/test_smogmap_scrapper.py
+++ tests/scrapping/test_smogmap_scrapper.py
@@ -1,7 +1,9 @@
 import pytest
 from pytest_mock import MockerFixture
 
+raise NotImplementedError()
 
+# FIXME ADD GLOBAL TESTS FOR PARSE_URLS TO SEE IF THE LIST IS FLAT!
 
 def main() -> None:
     import sys
