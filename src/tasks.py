ea832069eed842ba202ddb47fc7ad6430c91a978
NareszcieCosSieDzieje <paulcheater@gmail.com>
2022-04-30 17:38:58 +0200

    Fixed tasks to work with Redis

--- src/tasks.py
+++ src/tasks.py
@@ -1,34 +1,49 @@
-import dramatiq
-from dramatiq.brokers.redis import RedisBroker
 import logging
-from scrapping.scrapper import SmogScrapper, GovScrapper, SmogMapScrapper
+import dramatiq
+from dramatiq.results import Results
+from dramatiq.brokers.redis import RedisBroker
+from dramatiq.results.backends import RedisBackend
+from peewee import SqliteDatabase
+from scrapping.scrapper import GovScrapper, SmogMapScrapper
 from models.schema import (
     sqlite_db,
     Email,
     Smog,
-    smog_factory
 )
 from config import (
     RedisConfig,
+    DatabaseConfig,
 )
 
 logger: logging.Logger = logging.getLogger(__name__)
 
 # Code run by a dramatiq worker
-
-redis_broker = RedisBroker(
-    url=RedisConfig.REDIS_URL,
+redis_broker: RedisBroker = RedisBroker(
+    host=RedisConfig.REDIS_HOST,
+    port=RedisConfig.REDIS_PORT,
+    # url=RedisConfig.REDIS_URL,
 )
-#, middleware=[]) # middleware = [] to remove prometheus
+results_backend: RedisBackend = RedisBackend(
+    host=RedisConfig.REDIS_HOST,
+    port=RedisConfig.REDIS_PORT,
+    # url=RedisConfig.REDIS_URL,
+)
+redis_broker.add_middleware(
+    Results(backend=results_backend)
+)
 dramatiq.set_broker(redis_broker)
 
+
+smog_db_path: str = DatabaseConfig.DB_PATH
+database = SqliteDatabase(smog_db_path)
+sqlite_db.initialize(database)
 
 scrappers: tuple[GovScrapper, SmogMapScrapper] = (
     GovScrapper(),
     SmogMapScrapper(),
 )
 
-
+# either can migrate later to postgres or try to retrieve data from redis
 # @dramatiq.actor(store_results=True)
 @dramatiq.actor
 def do_smog_scrapping():
@@ -37,8 +52,7 @@ def do_smog_scrapping():
         parsed_smog_list.extend(scrapper.parse_urls())
     if parsed_smog_list:
         with sqlite_db.atomic():
-            # FIXME WRZUC DO BAZY CZY ZWROC JAKIEGOS JSONA?
-            Smog.bulk_create(parsed_smog_list, batch_size=10)  # FIXME OR UPDATE AS WELL?
+            Smog.bulk_create(parsed_smog_list, batch_size=10)
 
 
 def main() -> None:
